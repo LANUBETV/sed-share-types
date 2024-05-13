e9b80ce005bed93a21803cea686d866fcef21a62
Carlos García <carlosgarcia.cagm@gmail.com>
2024-05-12 22:51:29 -0500

    Revert "Refactor BaseEntity property names and add new types for files and us…" (#3)
    This reverts commit 8e74c9bace4c7a199c164a10dd3ed41a382a3378.

--- src/types/posts.ts
+++ src/types/posts.ts
@@ -1,59 +1,59 @@
 import { BaseEntity } from "../shared/base.entities";
-import { StatusFiles } from "./files";
 
 export interface Posts extends BaseEntity {
-  _id?: string;
+  id: string;
   createdBy: string;
-  folderId?: string[];
-  organizationId: string;
-  metadata: PostsMetadata
-  invoiceId?: string;
-  parentId?: string;
+  folderId: string;
+  metadata: {
+    tag: string;
+    title: string;
+    announcer: string | null;
+    organization: { name: string; id: string };
+    responsable: string | null;
+    version: string | null;
+    duration: string;
+    product: string | null;
+    orderPattern?: string | null;
+    city: string | null;
+    comment?: string | null;
+  };
   type: string;
-  status: Partial<StatusPosts>;
-  destionations?: Destinations[];
-  files?: FilesDestionations[];
-  historic?: Historic[];
-}
-
-interface Historic {
-  _id?: string;
-  dateTime: Date;
-  user: { id: string; name: string };
-  description: string;
-  fileId?: string;
-}
-
-export interface PostsMetadata {
-  title: string;
-  duration: string;
-  tags?: string[];
-  announcer?: string | null;
-  organization?: { name: string; id: string };
-  responsable?: string | null;
-  version?: string | null;
-  product?: string | null;
-  orderPattern?: string | null;
-  city?: string | null;
-  comment?: string | null;
+  status: StatusPosts;
+  files: Files[];
+  destionations: Destinations[];
 }
 
 interface Destinations {
   id: string;
-  name?: string;
-  status?: Number;
-  quality?: string;
-  postId?: string ;
-  fileId?: string;
+  name: string;
+  fileId: string;
 }
 
-interface FilesDestionations extends BaseEntity {
+interface Files {
   id: string;
-  status: StatusFiles;
-  version: number;
   name: string;
   path: string;
-  filename: string;
+  size: number;
+  standard: number;
+  format: string;
+  width: number;
+  height: number;
+  duration: number;
+  frameRate: number;
+  bitRate: number;
+  codec: string;
+  aspectRatio: string;
+  audioChannels: number;
+  sampleRate: number;
+  extension: string;
+  status: StatusFiles;
+}
+
+interface StatusFiles {
+  approved: number;
+  playable: boolean;
+  downloaded: boolean;
+  deleted: boolean;
 }
 
 interface StatusPosts {
@@ -64,6 +64,6 @@ interface StatusPosts {
   deleted: boolean;
   marked: boolean;
   file: boolean;
-  approved: boolean;
+  approved: number;
   durationAlert: boolean;
 }
