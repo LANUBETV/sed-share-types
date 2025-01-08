acc901558fb681d6686ece168dd18df59fb1e700
jbarcosdev <jbarcos.dev@gmail.com>
2025-01-08 01:34:22 -0500

    feat: Add PostStatus enum and labels to manage post statuses

--- src/types/posts.ts
+++ src/types/posts.ts
@@ -72,23 +72,3 @@ interface StatusPosts {
   approved: boolean;
   durationAlert: boolean;
 }
-
-export enum PostStatus {
-  DRAFT,
-  PENDING,
-  APPROVED,
-  REJECTED,
-  SENT,
-  DELETED,
-  RECEPTED,
-}
-
-export const PostStatusLabels: { [key in PostStatus]: string } = {
-  [PostStatus.DRAFT]: 'Borrador',
-  [PostStatus.PENDING]: 'Pendiente',
-  [PostStatus.APPROVED]: 'Aprobado',
-  [PostStatus.REJECTED]: 'Rechazado',
-  [PostStatus.SENT]: 'Enviado',
-  [PostStatus.DELETED]: 'Eliminado',
-  [PostStatus.RECEPTED]: 'Recibido',
-};
